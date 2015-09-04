c5167a1e9cebba9761e5d4f63640856b4afb12c4
Robert Morris <robert.f.morris@gmail.com>
2015-09-04 16:16:32 -0400

    added install_requires to setup

--- setup.py
+++ setup.py
@@ -5,6 +5,11 @@ setup(
     name='opsworks-easy-deploy',
     version='1.0.0',
     packages=['easy_deploy'],
+    install_requires=[
+        'botocore==1.2.0',
+        'click==5.1',
+        'arrow==0.6.0',
+    ],
     entry_points={
         'console_scripts': [
             'opsworks-easy-deploy = easy_deploy.easy_deploy:main',
