2ff1288e86ee8ff88ba353f865eac38d977c0695
Robert Morris <robert.f.morris@gmail.com>
2016-03-10 23:28:10 -0500

    bump version

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup
 
 setup(
     name='opsworks-easy-deploy',
-    version='1.1.0',
+    version='1.2.0',
     packages=['easy_deploy'],
     install_requires=[
         'botocore==1.2.0',
